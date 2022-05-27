4a198be0697aa81bfa514be219b6c06ea4c8da3f
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-27 17:56:23 +0100

    Updated paths for API calls

--- src/api/character.js
+++ src/api/character.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export function fetchAllCharacters() {
-  return axios.get("/api/character/");
+  return axios.get(`${process.env.PUBLIC_URL}/api/character/`);
 }
 
 export function getCharacterDetails(ID) {
-  return axios.get(`/api/character/${ID}`);
+  return axios.get(`${process.env.PUBLIC_URL}/api/character/${ID}`);
 }
