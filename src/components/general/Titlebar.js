b5398428324d4c986379a27d1c6e713f8af02052
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-28 18:53:53 +0100

    Disabled some dead links

--- src/components/general/Titlebar.js
+++ src/components/general/Titlebar.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
-import { Button, IconButton } from "@mui/material";
+import { Button, IconButton, Tooltip } from "@mui/material";
 
 import MenuIcon from "@mui/icons-material/Menu";
 
@@ -10,9 +10,10 @@ export default function Titlebar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ width: "100vw" }}>
         <Toolbar variant="dense">
           <IconButton
+            disabled
             size="large"
             edge="start"
             color="inherit"
@@ -28,19 +29,33 @@ export default function Titlebar() {
           >
             Moonsea Codex
           </Typography>
-          <Button color="inherit" onClick={() => navigate("/")}>
-            Dashboard
-          </Button>
-          <Button color="inherit" onClick={() => navigate("/tradingpost")}>
-            Trading Post
-          </Button>
-          <Button color="inherit" onClick={() => navigate("/itemvault")}>
-            Item Vault
-          </Button>
-          <Button color="inherit" onClick={() => navigate("/dungeonmaster")}>
-            DM Records
-          </Button>
-          <Button color="inherit">Login</Button>
+          <Box marginRight={"1em"}>
+            <Button color="inherit" onClick={() => navigate("/")}>
+              Dashboard
+            </Button>
+            <Tooltip title="Not yet implemented">
+              <Button
+                disabled
+                color="inherit"
+                onClick={() => navigate("/tradingpost")}
+              >
+                Trading Post
+              </Button>
+            </Tooltip>
+            <Tooltip title="Not yet implemented">
+              <Button
+                disabled
+                color="inherit"
+                onClick={() => navigate("/itemvault")}
+              >
+                Item Vault
+              </Button>
+            </Tooltip>
+            <Button color="inherit" onClick={() => navigate("/dungeonmaster")}>
+              DM Records
+            </Button>
+            <Button color="inherit">Login</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
