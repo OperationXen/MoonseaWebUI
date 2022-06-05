8ef3dd2e283b952d4bab36a4f40aa1959278afcd
OperationXen <operationxen@apocryphasecurity.com>
2022-06-05 20:48:03 +0100

    Iterated over session handling at the app level (#17)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import CharacterDetailWindow from "./components/characters/CharacterDetailWindow";
 import DungeonMasterWindow from "./components/dungeonmaster/DungeonMasterWindow";
@@ -10,17 +11,30 @@ import LoginWindow from "./components/user/LoginWindow";
 import Titlebar from "./components/general/Titlebar";
 import DataManager from "./datamanager/DataManager";
 
+import userStore from "./datastore/user";
+
 export default function App() {
+  const authenticated = userStore((s) => s.authenticated);
+
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Titlebar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<LoginWindow />} />
-        <Route path="/register" element={<RegistrationWindow />} />
+        {(!authenticated && (
+          <React.Fragment>
+            <Route path="/login" element={<LoginWindow />} />
+            <Route path="/register" element={<RegistrationWindow />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </React.Fragment>
+        )) || (
+          <React.Fragment>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/itemvault" element={<ItemVaultWindow />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </React.Fragment>
+        )}
         <Route path="/character/:id" element={<CharacterDetailWindow />} />
         <Route path="/dungeonmaster/:id" element={<DungeonMasterWindow />} />
-        <Route path="/itemvault" element={<ItemVaultWindow />} />
       </Routes>
       <FeedbackBar />
       <DataManager />
