505efbf740a030fcb03f63812e513533d58f23bf
OperationXen <operationxen@apocryphasecurity.com>
2022-06-23 22:41:29 +0100

    Iterate characters (#34)
    * Added placeholder token
    
    * Update calculated level on class changes
    
    * Trying different approach to button

--- src/components/characters/CharacterLevelsPane.js
+++ src/components/characters/CharacterLevelsPane.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
-import { Box, Fab, Tooltip, Typography } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import { Box, Button, Typography } from "@mui/material";
 
 import { updateCharacter } from "../../api/character";
 import useCharacterStore from "../../datastore/character";
@@ -11,17 +10,19 @@ import ClassChipWidget from "./widgets/ClassChipWidget";
 
 export default function CharacterLevelsPane() {
   const displayMessage = useSnackbar((s) => s.displayMessage);
-  const [uuid, classes, setClasses] = useCharacterStore((s) => [
-    s.uuid,
+  const uuid = useCharacterStore((s) => s.uuid);
+  const [classes, setClasses] = useCharacterStore((s) => [
     s.classes,
     s.setClasses,
   ]);
+  const setLevel = useCharacterStore((s) => s.setLevel);
   const [levelOpen, setLevelOpen] = useState(false);
 
   const handleEditClose = () => {
     setLevelOpen(false);
     updateCharacter(uuid, { classes: classes })
-      .then(() => {
+      .then((response) => {
+        setLevel(response.data.level);
         displayMessage("Updated character classes and levels", "info");
       })
       .catch(() => "Error updating character classes", "error");
@@ -32,31 +33,42 @@ export default function CharacterLevelsPane() {
       <Box
         sx={{
           display: "flex",
-          flexFlow: "row wrap",
-          alignItems: "center",
+          flexDirection: "column",
           justifyContent: "center",
-          gap: "0.4em",
-          minHeight: "3em",
         }}
       >
-        {(classes &&
-          classes.map((item) => {
-            return (
-              <ClassChipWidget data={item} onClick={() => setLevelOpen(true)} />
-            );
-          })) || (
-          <Typography variant="caption">No character classes</Typography>
-        )}
-        <Tooltip title="Edit classes">
-          <Fab
-            onClick={() => setLevelOpen(true)}
-            size="small"
-            color="primary"
-            sx={{ alignSelf: "center", marginLeft: "auto" }}
-          >
-            <AddIcon />
-          </Fab>
-        </Tooltip>
+        <Box
+          sx={{
+            display: "flex",
+            flexFlow: "row wrap",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "0.4em",
+            minHeight: "3em",
+          }}
+        >
+          {(classes &&
+            classes.map((item) => {
+              return (
+                <ClassChipWidget
+                  data={item}
+                  onClick={() => setLevelOpen(true)}
+                />
+              );
+            })) || (
+            <Typography variant="caption">No character classes</Typography>
+          )}
+        </Box>
+
+        <Button
+          variant="outlined"
+          onClick={() => setLevelOpen(true)}
+          size="small"
+          color="primary"
+          sx={{ alignSelf: "center" }}
+        >
+          Manage classes
+        </Button>
       </Box>
       {levelOpen && (
         <CharacterLevelEditDialog
