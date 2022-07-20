325ee3228b066736379f3987cdfc53026d092295
OperationXen <OperationXen@apocryphasecurity.com>
2022-07-20 16:31:33 +0100

    Added popup menu to profile button

--- src/components/user/ProfileWidget.js
+++ src/components/user/ProfileWidget.js
@@ -1,10 +1,22 @@
-import { Avatar } from "@mui/material";
+import { useState } from "react";
+
+import { Avatar, Menu, MenuItem } from "@mui/material";
 import { blue } from "@mui/material/colors";
 
 import userStore from "../../datastore/user";
 
 export default function ProfileWidget() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const menuOpen = Boolean(anchorEl);
   const { username } = userStore.getState();
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = (event) => {
+    setAnchorEl(null);
+    event.stopPropagation();
+  };
 
   return (
     <Avatar
@@ -14,8 +26,18 @@ export default function ProfileWidget() {
         boxShadow: "0px 0px 4px",
         "&:hover": { boxShadow: "0px 0px 6px" },
       }}
+      onClick={handleMenuOpen}
     >
       {username[0].toUpperCase() || "?"}
+      <Menu
+        id="profile-menu"
+        anchorEl={anchorEl}
+        open={menuOpen}
+        onClose={handleMenuClose}
+      >
+        <MenuItem>Profile Management</MenuItem>
+        <MenuItem>Log Out</MenuItem>
+      </Menu>
     </Avatar>
   );
 }
