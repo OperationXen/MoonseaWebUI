4ee9e3e521162fc20bc12503488d59375284995d
OperationXen <operationxen@apocryphasecurity.com>
2022-09-09 20:44:30 +0100

    Added loading widgets (#168)

--- src/components/trading/TradingPostItems.js
+++ src/components/trading/TradingPostItems.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import Grid from "@mui/material/Unstable_Grid2";
 
@@ -6,20 +6,25 @@ import EmptyAdvertsWidget from "./widgets/EmptyAdvertsWidget";
 import useTradeStore from "../../datastore/trade";
 import { getUserAdverts } from "../../api/trade";
 import TradeAdvert from "./TradeAdvert";
+import LoadingOverlay from "../general/LoadingOverlay";
 
 export default function TradingPostItems(props) {
   const [adverts, setAdverts] = useTradeStore((s) => [s.adverts, s.setAdverts]);
+  const [loading, setLoading] = useState(true);
   const refreshAdverts = useTradeStore((s) => s.refresh);
 
   useEffect(() => {
-    getUserAdverts().then((response) => {
-      setAdverts(response.data);
-    });
+    getUserAdverts()
+      .then((response) => {
+        setAdverts(response.data);
+      })
+      .finally(() => setLoading(false));
   }, [refreshAdverts, setAdverts]);
 
   if (adverts && adverts.length) {
     return (
       <Grid container sx={{ margin: "0.4em" }} spacing={2}>
+        <LoadingOverlay show={loading} />
         {adverts.map((advert) => {
           return (
             <Grid md={6} lg={4} xs={12}>
