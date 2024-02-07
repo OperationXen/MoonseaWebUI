033310d68e20e30865ed76ea2a7224c8e22ea57f
OperationXen <OperationXen@apocryphasecurity.com>
2024-02-07 19:56:26 +0000

    Added season 12d rewards

--- src/components/dungeonmaster/rewards/SeasonRewards.js
+++ src/components/dungeonmaster/rewards/SeasonRewards.js
@@ -9,6 +9,7 @@ import rewardData11b from "../season11b.json";
 import rewardData12a from "../season12a.json";
 import rewardData12b from "../season12b.json";
 import rewardData12c from "../season12c.json";
+import rewardData12d from "../season12d.json";
 
 export default function SeasonRewards(props) {
   const { allowUpdates, dmUUID, hours, onChange } = props;
@@ -16,7 +17,7 @@ export default function SeasonRewards(props) {
   const [rewards, setRewards] = useState(rewardData11b);
   const [selectedReward, setSelectedReward] = useState({});
   const [selectRewardOpen, setSelectRewardOpen] = useState(false);
-  const [season, setSeason] = useState("12c");
+  const [season, setSeason] = useState("12d");
 
   useEffect(() => {
     if (season === "11a") setRewards(rewardData11a);
@@ -24,6 +25,7 @@ export default function SeasonRewards(props) {
     if (season === "12a") setRewards(rewardData12a);
     if (season === "12b") setRewards(rewardData12b);
     if (season === "12c") setRewards(rewardData12c);
+    if (season === "12d") setRewards(rewardData12d);
   }, [season]);
 
   const handleSelect = (reward) => {
@@ -42,6 +44,7 @@ export default function SeasonRewards(props) {
           <MenuItem value={"12a"}>Season 12A</MenuItem>
           <MenuItem value={"12b"}>Season 12B</MenuItem>
           <MenuItem value={"12c"}>Season 12C</MenuItem>
+          <MenuItem value={"12d"}>Season 12D</MenuItem>
         </Select>
       </FormControl>
       <Box width={"100%"} justifyContent={"space-around"} sx={{ flexFlow: "column", overflowY: "scroll" }}>
