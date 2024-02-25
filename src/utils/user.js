51c0ba9e1a67c16a220a690080f6ddfd0cea5b10
OperationXen <OperationXen@apocryphasecurity.com>
2024-02-25 15:56:12 +0000

    Update regex

--- src/utils/user.js
+++ src/utils/user.js
@@ -6,6 +6,6 @@ export function validatePassword(pass) {
 export function checkDiscordID(discordID) {
   if (!discordID) return false;
 
-  let retval = !!discordID.match(/^[\w ]+\w$/);
+  let retval = !!discordID.match(/^(?=.{2,32}$)(?!(?:everyone|here)$)\.?[a-z0-9_]+(?:\.[a-z0-9_]+)*\.?$/);
   return retval;
 }
