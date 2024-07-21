933107c59beab651e7d917a8c575249393b075e6
OperationXen <operationxen@apocryphasecurity.com>
2024-07-21 15:34:41 +0100

    Updates to allow for multiple item creation (#218)

--- src/components/events/character_event_panes/CreateCharacterGame.js
+++ src/components/events/character_event_panes/CreateCharacterGame.js
@@ -4,15 +4,17 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
 import { Box, Typography, Button, FormGroup, Tooltip } from "@mui/material";
 import { Checkbox, TextField, Divider, FormControlLabel } from "@mui/material";
-import { MenuItem, Select, FormControl } from "@mui/material";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
+import AddIcon from "@mui/icons-material/Add";
 import { default as DowntimeIcon } from "@mui/icons-material/Hotel";
 import { GiTwoCoins } from "react-icons/gi";
 
 import useSnackbar from "../../../datastore/snackbar";
-import useCharacterStore from "../../../datastore/character";
 import StatsWidget from "../../characters/widgets/StatsWidget";
+import SimpleItemCreateWidget from "./SimpleItemCreateWidget";
+import useCharacterStore from "../../../datastore/character";
+
 import { createPlayerGame } from "../../../api/events";
 
 const row = {
@@ -37,10 +39,27 @@ export default function CreateCharacterGame(props) {
   const [level, setLevel] = useState(true);
   const [gold, setGold] = useState(250);
   const [downtime, setDowntime] = useState(10);
-  const [itemText, setItemText] = useState("");
-  const [rarity, setRarity] = useState("uncommon");
+  const [items, setItems] = useState([{ name: "", rarity: "uncommon" }]);
 
   const [highlight, setHighlight] = useState(false);
+
+  const handleAddItem = () => {
+    setItems([...items, { name: "", rarity: "uncommon" }]);
+  };
+  const handleItemDelete = (index) => {
+    items.splice(index, 1);
+    setItems([...items]);
+  };
+  const handleItemNameChange = (index, newName) => {
+    const newItems = [...items];
+    newItems[index].name = newName;
+    setItems(newItems);
+  };
+  const handleItemRarityChange = (index, newRarity) => {
+    const newItems = [...items];
+    newItems[index].rarity = newRarity;
+    setItems(newItems);
+  };
 
   const handleSubmit = () => {
     createPlayerGame(characterUUID, {
@@ -52,8 +71,7 @@ export default function CreateCharacterGame(props) {
       gold: gold,
       downtime: downtime,
       levels: level ? 1 : 0,
-      item: itemText,
-      rarity: rarity,
+      items: items,
     })
       .then((response) => {
         displayMessage("Game added to log", "success");
@@ -106,9 +124,7 @@ export default function CreateCharacterGame(props) {
             inputFormat="yyyy/MM/dd"
             value={date}
             onChange={setDate}
-            renderInput={(params) => (
-              <TextField {...params} sx={{ maxWidth: "30%" }} />
-            )}
+            renderInput={(params) => <TextField {...params} sx={{ maxWidth: "30%" }} />}
           />
         </LocalizationProvider>
       </Box>
@@ -144,9 +160,7 @@ export default function CreateCharacterGame(props) {
         />
         <FormGroup>
           <FormControlLabel
-            control={
-              <Checkbox checked={level} onChange={() => setLevel(!level)} />
-            }
+            control={<Checkbox checked={level} onChange={() => setLevel(!level)} />}
             label="Level Gained"
           />
         </FormGroup>
@@ -160,33 +174,33 @@ export default function CreateCharacterGame(props) {
           sx={{ width: "25%" }}
         />
       </Box>
+
+      <Divider sx={{ margin: "0.4em" }} />
+      {items.map((item, index) => {
+        return (
+          <SimpleItemCreateWidget
+            id={index}
+            key={index}
+            name={item.name}
+            rarity={item.rarity}
+            setName={handleItemNameChange}
+            setRarity={handleItemRarityChange}
+            handleDelete={() => handleItemDelete(index)}
+          />
+        );
+      })}
+
+      <Button startIcon={<AddIcon />} variant="outlined" size="small" fullWidth onClick={handleAddItem}>
+        {items.length >= 1 ? "Add Another item" : "Add an item"}
+      </Button>
+
       <Box sx={row}>
-        <TextField
-          sx={{ flexGrow: 1 }}
-          label="Item Reward"
-          value={itemText}
-          onChange={(e) => setItemText(e.target.value)}
-        />
-        <FormControl sx={{ minWidth: "12em" }}>
-          <Select value={rarity} onChange={(e) => setRarity(e.target.value)}>
-            <MenuItem value="uncommon">Uncommon</MenuItem>
-            <MenuItem value="rare">Rare</MenuItem>
-            <MenuItem value="veryrare">Very Rare</MenuItem>
-            <MenuItem value="legendary">Legendary</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <Box sx={row}>
-        <Tooltip title="No implemented yet" placement="left">
+        <Tooltip title="Not implemented yet" placement="left">
           <TextField fullWidth label="Consumables" disabled />
         </Tooltip>
       </Box>
 
-      <Box
-        display="flex"
-        onMouseOver={() => setHighlight(true)}
-        onMouseOut={() => setHighlight(false)}
-      >
+      <Box display="flex" onMouseOver={() => setHighlight(true)} onMouseOut={() => setHighlight(false)}>
         <Button
           variant="contained"
           sx={{ width: "60%", margin: "auto" }}
