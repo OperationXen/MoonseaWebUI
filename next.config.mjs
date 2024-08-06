de89d0ce6ad604e46ee442849e8347fb0e51c305
OperationXen <OperationXen@apocryphasecurity.com>
2024-08-06 22:00:15 +0100

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 
 const nextConfig = {
   output: "standalone", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
 
   async rewrites() {
     return [
