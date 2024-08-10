cf3152c01c95855de5fb11b2a3cc59e8d4a5b16d
OperationXen <operationxen@apocryphasecurity.com>
2024-08-11 00:33:23 +0100

    243 fix build (#244)
    * Updated
    
    * better

--- src/datastore/magicitem.js
+++ src/datastore/magicitem.js
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 
 const useMagicItemStore = create((set) => ({
   refresh: 0,
--- src/datastore/snackbar.js
+++ src/datastore/snackbar.js
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 
 const useSnackbar = create((set) => ({
   open: false,
