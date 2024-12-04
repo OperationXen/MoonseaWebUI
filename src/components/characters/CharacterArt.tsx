48ca8a5af078136551335942953c112aaa9b4374
OperationXen <OperationXen@apocryphasecurity.com>
2024-12-04 22:03:17 +0000

    Added functionality to upload images

--- src/components/characters/CharacterArt.tsx
+++ src/components/characters/CharacterArt.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useFilePicker } from "use-file-picker";
 
 import Image from "next/image";
 
@@ -7,25 +8,57 @@ import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { Zoom, Fab, Tooltip } from "@mui/material";
 import { Box, Button, ButtonGroup, Typography } from "@mui/material";
 
+import { uploadCharacterImage } from "@/api/character";
+import useSnackbar from "@/datastore/snackbar";
+
 import { Character } from "@/types/character";
 
-type ImageTypes = "art" | "token";
+type ImageTypes = "artwork" | "token";
 type PropsType = {
   character: Character;
   updateCharacter: (x: Partial<Character>) => Promise<any>;
 };
 
 export function CharacterArt(props: PropsType) {
-  const { character } = props;
+  const { character, updateCharacter } = props;
 
+  const displayMessage = useSnackbar((s) => s.displayMessage);
+  const [openFileSelector, { filesContent, clear, errors }] = useFilePicker({
+    readAs: "DataURL",
+    accept: "image/*",
+    multiple: false,
+    maxFileSize: 0.8,
+  });
   const [show, setShow] = useState<ImageTypes>("token");
   const [active, setActive] = useState(false);
 
   const getImageLink = () => {
     if (character.token && show === "token") return character.token;
-    if (character.artwork && show === "art") return character.artwork;
+    if (character.artwork && show === "artwork") return character.artwork;
     return "";
   };
+
+  //when file information set
+  useEffect(() => {
+    if (filesContent.length) {
+      uploadCharacterImage(character.uuid, show, filesContent[0])
+        .then((response) => {
+          if (show === "artwork") updateCharacter({ artwork: response.data.path });
+          else updateCharacter({ token: response.data.path });
+          displayMessage(`${response.data.message}`, "success");
+        })
+        .finally(() => {
+          clear();
+        });
+    }
+  }, [filesContent, character.uuid, displayMessage, show, clear]);
+
+  // when an error occurs with file upload
+  useEffect(() => {
+    if (errors.length) {
+      if (errors[0].fileSizeToolarge) displayMessage(`Failed to upload, file is too big`, "error");
+    }
+  }, [errors]);
 
   return (
     <Box
@@ -41,13 +74,24 @@ export function CharacterArt(props: PropsType) {
       onMouseEnter={() => setActive(true)}
       onMouseLeave={() => setActive(false)}
     >
-      <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center", alignItems: "center" }}>
-        {(getImageLink() && <Image src={getImageLink()} alt={`${character.name}-${show}`} />) || (
-          <Typography>Image not set</Typography>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingY: "4px",
+          height: 256,
+        }}
+      >
+        {(getImageLink() && (
+          <Image src={getImageLink()} alt={`${character.name}-${show}`} width={242} height={242} />
+        )) || (
+          <Typography sx={{ opacity: "30%" }}>{`${show === "token" ? "Token" : "Artwork"} image not set`}</Typography>
         )}
       </Box>
       <ButtonGroup color="inherit" size="small" variant="text" fullWidth>
-        <Button onClick={() => setShow("art")} sx={{ paddingY: "1px", opacity: show === "art" ? 1 : 0.2 }}>
+        <Button onClick={() => setShow("artwork")} sx={{ paddingY: "1px", opacity: show === "artwork" ? 1 : 0.2 }}>
           Artwork
         </Button>
         <Button onClick={() => setShow("token")} sx={{ paddingY: "1px", opacity: show === "token" ? 1 : 0.2 }}>
@@ -57,7 +101,7 @@ export function CharacterArt(props: PropsType) {
 
       <Zoom in={active}>
         <Tooltip title="Upload image">
-          <Fab size="small" sx={{ position: "absolute", right: "4px", top: "4px" }}>
+          <Fab size="small" sx={{ position: "absolute", right: "4px", top: "4px" }} onClick={openFileSelector}>
             <FileUploadIcon />
           </Fab>
         </Tooltip>
