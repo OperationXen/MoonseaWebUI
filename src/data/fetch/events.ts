559a914ad09aa40d0b32929fc6c85a617f1f7b63
OperationXen <OperationXen@apocryphasecurity.com>
2024-12-28 19:33:50 +0000

    Add skeleton of optimistic update stuff

--- src/data/fetch/events.ts
+++ src/data/fetch/events.ts
@@ -41,6 +41,29 @@ export function useEvents(characterUUID: UUID) {
   const createEvent = useMutation({
     mutationFn: (data: Partial<CharacterEvent | GameEvent>) =>
       createEventFn(characterUUID, data),
+
+    //     onMutate: async (newChar: Partial<Character>) => {
+    //       // Cancel any outgoing refetches to avoid overwriting optimistic update
+    //       await queryClient.cancelQueries({
+    //         queryKey: ["character", newChar.uuid],
+    //       });
+    //       // Snapshot the previous value
+    //       const oldCharData = queryClient.getQueryData(["character", newChar.uuid]);
+    //       // Optimistically update to the new value
+    //       queryClient.setQueryData(["character", newChar.uuid], newChar);
+    //       // Return a context with the old data and the new
+    //       return { oldCharData, newChar };
+    //     },
+    //     // If the mutation fails, use the context we returned above
+    //     onError: (_err, _newChar: Partial<Character>, context) => {
+    //       if (context) {
+    //         queryClient.setQueryData(
+    //           ["character", context.newChar.uuid],
+    //           context.oldCharData,
+    //         );
+    //       }
+    //     },
+
     onSettled: () =>
       queryClient.invalidateQueries({
         queryKey: ["events", "all", "character", characterUUID],
@@ -49,67 +72,3 @@ export function useEvents(characterUUID: UUID) {
 
   return { ...fetchData, createEvent: createEvent.mutateAsync };
 }
-
-// export async function getCharacter(ID: string) {
-//   return api.get(`/api/data/character/${ID}`).then((r) => {
-//     return r.data as Character;
-//   });
-// }
-
-// export async function updateCharacterFn(data: Partial<Character>) {
-//   return api
-//     .patch(`/api/data/character/${data.uuid}/`, { ...data })
-//     .then((r) => r.data as Character);
-// }
-
-// export function useCharacters() {
-//   return useQuery({
-//     queryKey: ["characters"],
-//     queryFn: () => getCharacters(),
-//   });
-// }
-
-// export function useCharacter(uuid: UUID) {
-//   const queryClient = useQueryClient();
-
-//   const fetchData = useQuery({
-//     queryKey: ["character", uuid],
-//     queryFn: () => getCharacter(uuid),
-//   });
-
-//   const updateCharacter = useMutation({
-//     mutationFn: (charData: Partial<Character>) =>
-//       updateCharacterFn({ ...charData, uuid: uuid }),
-//     onMutate: async (newChar: Partial<Character>) => {
-//       // Cancel any outgoing refetches to avoid overwriting optimistic update
-//       await queryClient.cancelQueries({
-//         queryKey: ["character", newChar.uuid],
-//       });
-//       // Snapshot the previous value
-//       const oldCharData = queryClient.getQueryData(["character", newChar.uuid]);
-//       // Optimistically update to the new value
-//       queryClient.setQueryData(["character", newChar.uuid], newChar);
-//       // Return a context with the old data and the new
-//       return { oldCharData, newChar };
-//     },
-//     // If the mutation fails, use the context we returned above
-//     onError: (_err, _newChar: Partial<Character>, context) => {
-//       if (context) {
-//         queryClient.setQueryData(
-//           ["character", context.newChar.uuid],
-//           context.oldCharData,
-//         );
-//       }
-//     },
-//     // Always refetch after error or success:
-//     onSettled: (newChar: Character | undefined) => {
-//       if (newChar) {
-//         queryClient.invalidateQueries({
-//           queryKey: ["character", newChar.uuid],
-//         });
-//       }
-//     },
-//   });
-
-//   return { ...fetchData, updateCharacter: updateCharacter.mutateAsync };
-// }
