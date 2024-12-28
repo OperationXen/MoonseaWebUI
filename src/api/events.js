415f8252f86ba94166ea29f13beb31f45641eab5
OperationXen <OperationXen@apocryphasecurity.com>
2024-12-28 22:59:25 +0000

    Removed unused code

--- src/api/events.js
+++ src/api/events.js
@@ -1,10 +1,5 @@
 import api from "./base";
 
-// unpaginated fetch of all character events
-export function getEventsForCharacter(ID) {
-  return api.get(`/api/data/character_events/${ID}`);
-}
-
 // function expects an object - possibly should convert to typescript to prevent the inevitable
 export function createDMReward(data) {
   return api.post("/api/data/dm_reward", data);
@@ -61,37 +56,4 @@ export function updateDMGame(uuid, data) {
   let url = `/api/data/dm_game/${uuid}/`;
 
   return api.patch(url, data);
-}
-
-export function createPlayerGame(ID, gameData) {
-  debugger;
-  let data = { character_uuid: ID, ...gameData };
-
-  return api.post(`/api/data/game`, data);
-}
-
-export function removeCharacterGame(gameUUID, characterUUID) {
-  return api.delete(`/api/data/game/${gameUUID}/`, {
-    data: { character_uuid: characterUUID },
-  });
-}
-
-// spellbook events
-export function createEventSpellbookUpdate(
-  characterUUID,
-  gold,
-  downtime,
-  dm,
-  sourceChar,
-  spellsText,
-) {
-  let data = {
-    character_uuid: characterUUID,
-    gold: gold,
-    downtime: downtime,
-    dm: dm,
-    source: sourceChar,
-    spells: spellsText,
-  };
-  return api.post("/api/data/spellbook", data);
 }
