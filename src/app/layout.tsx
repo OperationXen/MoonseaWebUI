ba06993415bcf9bf88b12475e828582833a2a8e6
OperationXen <operationxen@apocryphasecurity.com>
2024-12-31 11:34:38 +0000

    306 flavicon (#307)
    * Added
    
    * Removed unused

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         <ReactQueryProvider>
           <StyledEngineProvider injectFirst>
