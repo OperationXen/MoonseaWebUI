919d4f091635c099f3b4adae27b8914f14da5c42
OperationXen <OperationXen@apocryphasecurity.com>
2025-01-01 14:42:17 +0000

    Added add/remove handling

--- src/components/items/magicitems/MagicItemsGrid.tsx
+++ src/components/items/magicitems/MagicItemsGrid.tsx
@@ -5,10 +5,10 @@ import React, { ChangeEvent, useState } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import TextsmsIcon from "@mui/icons-material/Textsms";
+import DisabledByDefaultIcon from "@mui/icons-material/DisabledByDefault";
 
 import { Box, Checkbox, IconButton, Tooltip, Typography } from "@mui/material";
 import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
-import { GridSlotsComponentsProps } from "@mui/x-data-grid";
 
 import { useMagicItems } from "@/data/fetch/items/magicitems";
 import { getNumberEquipped } from "@/utils/items";
@@ -47,15 +47,22 @@ export function MagicItemsGrid(props: PropsType) {
   const renderEquipped = (p: GridRenderCellParams<MagicItem>) => {
     return (
       <Box className="flex items-center h-full w-full justify-center">
-        <Checkbox
-          checked={p.row.equipped}
-          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-            updateItem({
-              uuid: p.row.uuid,
-              equipped: !!event.target.checked,
-            });
-          }}
-        />
+        {(p.row.market && (
+          <Tooltip title="This item is in the trading post, if you want to equip it you must first remove it from the trading post">
+            <DisabledByDefaultIcon className="opacity-80" />
+          </Tooltip>
+        )) || (
+          <Checkbox
+            disabled={p.row.market}
+            checked={p.row.equipped}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              updateItem({
+                uuid: p.row.uuid,
+                equipped: !!event.target.checked,
+              });
+            }}
+          />
+        )}
       </Box>
     );
   };
@@ -64,7 +71,7 @@ export function MagicItemsGrid(props: PropsType) {
     {
       field: "name",
       headerName: "Name",
-      flex: 5,
+      flex: 4,
     },
     {
       field: "description",
@@ -89,7 +96,7 @@ export function MagicItemsGrid(props: PropsType) {
       flex: 2,
       renderCell: (p) => {
         return (
-          <Box className="flex items-center h-full">
+          <Box className="flex items-center h-full justify-center">
             <RarityWidget rarity={p.row.rarity} text />
           </Box>
         );
@@ -97,11 +104,11 @@ export function MagicItemsGrid(props: PropsType) {
     },
     {
       field: "attunement",
-      headerName: "Attunement",
-      flex: 1,
+      headerName: `Attunement (${0})`,
+      flex: 2,
       renderCell: (p) => {
         return (
-          <Box className="flex items-center h-full">
+          <Box className="flex items-center h-full justify-center">
             <Typography>{p.row.attunement ? "Required" : ""}</Typography>
           </Box>
         );
@@ -109,24 +116,20 @@ export function MagicItemsGrid(props: PropsType) {
     },
     {
       field: "equipped",
-      headerName: `Equipped ${getNumberEquipped(magicItems)}`,
+      headerName: `Equipped (${getNumberEquipped(magicItems)})`,
       flex: 2,
       renderCell: renderEquipped,
     },
     {
       field: "market",
-      headerName: "Market",
+      headerName: "Trade",
       flex: 1,
       renderCell: (p) => {
-        return (
-          <Box className="w-full flex items-center justify-center">
-            <ItemMarketWidget item={p.row} />
-          </Box>
-        );
+        return <ItemMarketWidget item={p.row} />;
       },
     },
     {
-      field: "controls",
+      field: "Item edit controls",
       headerName: "",
       renderCell: (p) => {
         return (
--- src/components/items/widgets/ItemMarketWidget.tsx
+++ src/components/items/widgets/ItemMarketWidget.tsx
@@ -1,4 +1,8 @@
-import { IconButton, Tooltip } from "@mui/material";
+"use client";
+
+import { useState } from "react";
+
+import { IconButton, Tooltip, Box } from "@mui/material";
 
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
 import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
@@ -19,21 +23,49 @@ export function ItemMarketWidget(props: PropsType) {
   const { updateItem } = useMagicItem(item.uuid);
   const { displayMessage } = useSnackbar();
 
+  const [mouseOver, setMouseOver] = useState(false);
+
+  const getIcon = () => {
+    if (mouseOver && item.market)
+      return (
+        <Tooltip title="Remove item from trading post">
+          <RemoveShoppingCartIcon />
+        </Tooltip>
+      );
+    if (mouseOver && !item.market)
+      return (
+        <Tooltip title="Send item to trading post">
+          <ShoppingCartCheckoutIcon />
+        </Tooltip>
+      );
+    else
+      return (
+        <ShoppingCartIcon
+          color={item.market ? "primary" : "inherit"}
+          className={item.market ? "opacity-100" : "opacity-20"}
+        />
+      );
+  };
+
   return (
-    <IconButton
-      onClick={() => {
-        updateItem({ uuid: item.uuid, market: !item.market }).then(() => {
-          displayMessage(
-            `${item.name} ${item.market ? "sent to trading post" : "removed from trading post"}`,
-            "info",
-          );
-        });
-      }}
+    <Box
+      className="w-full h-full flex items-center justify-center"
+      onMouseEnter={() => setMouseOver(true)}
+      onMouseLeave={() => setMouseOver(false)}
     >
-      <ShoppingCartIcon
-        className={item.market ? "opacity-100" : "opacity-30"}
-      />
-    </IconButton>
+      <IconButton
+        onClick={() => {
+          updateItem({ uuid: item.uuid, market: !item.market }).then(() => {
+            displayMessage(
+              `${item.name} ${item.market ? "sent to trading post" : "removed from trading post"}`,
+              "info",
+            );
+          });
+        }}
+      >
+        {getIcon()}
+      </IconButton>
+    </Box>
   );
 }
 
