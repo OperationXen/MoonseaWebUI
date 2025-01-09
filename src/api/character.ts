01afac42ac30072be8e9d33c658eaabfa85cc5e4
Daemoxia <operationxen@apocryphasecurity.com>
2025-01-09 20:27:51 +0000

    330 characters view not updated on character creation (#333)
    * Moved to react-query
    
    * Removed unused code

--- src/api/character.ts
+++ src/api/character.ts
@@ -1,41 +1,7 @@
 import api from "./base";
 
-import type { Character, CharacterImageType } from "@/types/character";
+import type { CharacterImageType } from "@/types/character";
 import type { UUID } from "@/types/uuid";
-
-export async function getCharacters() {
-  return api
-    .get("/api/data/character")
-    .then((r) => r.data.results as Character[]);
-}
-
-export async function getCharacter(ID: string) {
-  return api
-    .get(`/api/data/character/${ID}`)
-    .then((r) => r.data.result as Character);
-}
-
-export async function updateCharacter2(data: Character) {
-  return api
-    .patch(`/api/data/characeter/${data.uuid}/`, data)
-    .then((r) => r.data.result as Character);
-}
-
-export function fetchAllCharacters() {
-  return api.get("/api/data/character");
-}
-
-export function getCharacterDetails(ID: any) {
-  return api.get(`/api/data/character/${ID}/`);
-}
-
-export function createCharacter(data: any) {
-  return api.post("/api/data/character", data);
-}
-
-export function updateCharacter(ID: any, data: any) {
-  return api.patch(`/api/data/character/${ID}/`, data);
-}
 
 export function deleteCharacter(ID: any) {
   return api.delete(`/api/data/character/${ID}/`);
