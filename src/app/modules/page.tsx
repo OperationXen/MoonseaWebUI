6c18349dfa474f9b9776fb0fdad10177d6240e39
OperationXen <OperationXen@apocryphasecurity.com>
2025-02-13 22:21:53 +0000

    Added modules to navbar

---
+++ src/app/modules/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Box, Container } from "@mui/material";
+
+export default function PlayerModules() {
+  return (
+    <Container sx={{ marginTop: "0.4em" }}>
+      <Box className="flex gap-2"></Box>
+    </Container>
+  );
+}
