990ab45d7885b0f964e84d98d4e4c5c7a6d91dbf
OperationXen <OperationXen@apocryphasecurity.com>
2025-02-20 15:43:36 +0000

    Redirect on fail to load

--- src/app/character/[characterUUID]/page.tsx
+++ src/app/character/[characterUUID]/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { Container, Box, Paper } from "@mui/material";
+import { ErrorBoundary } from "react-error-boundary";
+import { useRouter } from "next/navigation";
 
-import { useCharacter } from "@/data/fetch/character";
-import CharacterDetailsPane from "@/components/characters/CharacterDetailsPane";
-import LoadingOverlay from "@/components/general/LoadingOverlay";
-import CharacterEvents from "components/events/CharacterEvents";
-import CharacterArt from "@/components/characters/CharacterArt";
-import ItemPane from "components/items/ItemPane";
+import CharacterDetails from "@/components/characters/CharacterDetails";
+import useSnackbar from "@/datastore/snackbar";
 
-import type { Character } from "@/types/character";
 import type { UUID } from "@/types/uuid";
 
 type PropsType = {
@@ -19,57 +15,18 @@ type PropsType = {
 export default function CharacterPage(props: PropsType) {
   const { characterUUID } = props.params;
 
-  const {
-    data: characterData,
-    isPending,
-    updateCharacter,
-    refreshCharacter,
-  } = useCharacter(characterUUID);
-
-  if (isPending) return <LoadingOverlay open={true} />;
-  if (!characterData) return null;
-
-  const handleCharacterUpdate = (changes: Partial<Character>) => {
-    const newData = { ...characterData, ...changes };
-    return updateCharacter(newData);
-  };
+  const router = useRouter();
+  const { displayMessage } = useSnackbar();
 
   return (
-    <Container className="mt-4">
-      <Paper
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          border: "1px solid black",
-          borderRadius: "8px",
-          boxShadow: "1px 1px 5px 1px grey",
-          overflow: "hidden",
-          marginBottom: "0.4em",
-          minHeight: "calc(100vh - 5em)",
-        }}
-      >
-        <Box className="flex">
-          <CharacterArt
-            character={characterData}
-            refreshCharacter={refreshCharacter}
-          />
-          <CharacterDetailsPane
-            character={characterData}
-            updateCharacter={handleCharacterUpdate}
-          />
-        </Box>
-
-        <ItemPane data={characterData} />
-      </Paper>
-
-      <Box sx={{ minHeight: "50em", marginBottom: "0.4em" }}>
-        <CharacterEvents
-          characterUUID={characterUUID}
-          characterName={characterData?.name || ""}
-          downtime={characterData?.downtime || 0}
-          editable={characterData?.editable || false}
-        />
-      </Box>
-    </Container>
+    <ErrorBoundary
+      onError={() => {
+        displayMessage("Character not found", "error");
+        router.push("/characters");
+      }}
+      fallback={"Error"}
+    >
+      <CharacterDetails characterUUID={characterUUID} />
+    </ErrorBoundary>
   );
 }
---
+++ src/components/characters/CharacterDetails.tsx
@@ -0,0 +1,75 @@
+import { Container, Box, Paper } from "@mui/material";
+
+import { useCharacter } from "@/data/fetch/character";
+import CharacterDetailsPane from "@/components/characters/CharacterDetailsPane";
+import LoadingOverlay from "@/components/general/LoadingOverlay";
+import CharacterEvents from "components/events/CharacterEvents";
+import CharacterArt from "@/components/characters/CharacterArt";
+import ItemPane from "components/items/ItemPane";
+
+import type { Character } from "@/types/character";
+import type { UUID } from "@/types/uuid";
+
+type PropsType = {
+  characterUUID: UUID;
+};
+
+export function CharacterDetails(props: PropsType) {
+  const { characterUUID } = props;
+
+  const {
+    data: characterData,
+    isPending,
+    updateCharacter,
+    refreshCharacter,
+  } = useCharacter(characterUUID);
+
+  if (isPending) return <LoadingOverlay open={true} />;
+  if (!characterData) return null;
+
+  const handleCharacterUpdate = (changes: Partial<Character>) => {
+    const newData = { ...characterData, ...changes };
+    return updateCharacter(newData);
+  };
+
+  return (
+    <Container className="mt-4">
+      <Paper
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          border: "1px solid black",
+          borderRadius: "8px",
+          boxShadow: "1px 1px 5px 1px grey",
+          overflow: "hidden",
+          marginBottom: "0.4em",
+          minHeight: "calc(100vh - 5em)",
+        }}
+      >
+        <Box className="flex">
+          <CharacterArt
+            character={characterData}
+            refreshCharacter={refreshCharacter}
+          />
+          <CharacterDetailsPane
+            character={characterData}
+            updateCharacter={handleCharacterUpdate}
+          />
+        </Box>
+
+        <ItemPane data={characterData} />
+      </Paper>
+
+      <Box sx={{ minHeight: "50em", marginBottom: "0.4em" }}>
+        <CharacterEvents
+          characterUUID={characterUUID}
+          characterName={characterData?.name || ""}
+          downtime={characterData?.downtime || 0}
+          editable={characterData?.editable || false}
+        />
+      </Box>
+    </Container>
+  );
+}
+
+export default CharacterDetails;
