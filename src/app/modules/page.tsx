f135995f71afe9ca12f09f1d3372a13bf3015aea
OperationXen <OperationXen@apocryphasecurity.com>
2025-02-20 21:12:08 +0000

    Updated formatting

--- src/app/modules/page.tsx
+++ src/app/modules/page.tsx
@@ -2,26 +2,47 @@
 
 import { useState } from "react";
 
-import { Box, Container, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
+import { Box, TextField } from "@mui/material";
+import { Paper, Typography, InputAdornment } from "@mui/material";
+
+import AuthenticationRequired from "@/components/user/AuthenticationRequired";
 import ModulesGrid from "./ModulesGrid";
 
 export default function PlayerModules() {
   const [search, setSearch] = useState("");
 
   return (
-    <Container sx={{ marginTop: "0.4em" }}>
-      <Box className="flex flex-col gap-2 w-full">
-        <Box className="flex w-full">
+    <AuthenticationRequired>
+      <Paper className="flex flex-col flex-grow px-2 pb-2 m-2">
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          className="mt-2 mb-1"
+        >
+          <Typography variant="h5">Module library</Typography>
           <TextField
             className="basis-1/2 min-w-96"
+            size="small"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            label="Search"
+            label="Search played modules"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              },
+            }}
           />
         </Box>
+
         <ModulesGrid search={search} />
-      </Box>
-    </Container>
+      </Paper>
+    </AuthenticationRequired>
   );
 }
