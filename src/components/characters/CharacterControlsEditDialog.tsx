b24bf628c45411f5ff9a4c36c4b65ec75b775923
OperationXen <OperationXen@apocryphasecurity.com>
2025-05-04 14:52:46 +0100

    Added fields to character edit pane

--- src/components/characters/CharacterControlsEditDialog.tsx
+++ src/components/characters/CharacterControlsEditDialog.tsx
@@ -26,14 +26,25 @@ export default function CharacterControlsEditDialog(props: PropsType) {
   const [highlight, setHighlight] = useState(false);
   const [name, setName] = useState(character.name);
   const [race, setRace] = useState(character.race);
+  const [faction, setFaction] = useState(character.faction);
+  const [campaign, setCampaign] = useState(character.campaign);
+  const [bastion, setBastion] = useState(character.bastion);
   const [sheet, setSheet] = useState(character.sheet);
 
   const handleSave = () => {
-    updateCharacter({ ...character, name: name, race: race, sheet: sheet })
+    updateCharacter({
+      ...character,
+      name,
+      race,
+      sheet,
+      faction,
+      campaign,
+      bastion,
+    })
       .then(() => {
         displayMessage("Updated character details", "success");
       })
-      .catch(() => displayMessage(""));
+      .catch(() => displayMessage("Error updating character", "error"));
     onClose();
   };
 
@@ -41,17 +52,19 @@ export default function CharacterControlsEditDialog(props: PropsType) {
     <Dialog
       open={open}
       onClose={onClose}
-      PaperProps={{
-        sx: {
-          borderRadius: "8px",
-          border: "1px solid black",
-          boxShadow: `0 0 4px inset black`,
-          display: "flex",
-          width: "42em",
-          flexDirection: "column",
-          alignItems: "center",
-          padding: "1em 2em",
-          gap: "0.6em",
+      slotProps={{
+        paper: {
+          sx: {
+            borderRadius: "8px",
+            border: "1px solid black",
+            boxShadow: `0 0 4px inset black`,
+            display: "flex",
+            width: "42em",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "1em 2em",
+            gap: "0.6em",
+          },
         },
       }}
     >
@@ -61,6 +74,8 @@ export default function CharacterControlsEditDialog(props: PropsType) {
       <Divider sx={{ width: "95%" }}>Details</Divider>
       <TextField
         fullWidth
+        required
+        size="small"
         label="Character name"
         value={name}
         onChange={(e) => {
@@ -70,6 +85,8 @@ export default function CharacterControlsEditDialog(props: PropsType) {
       />
       <TextField
         fullWidth
+        required
+        size="small"
         label="Race"
         value={race}
         onChange={(e) => {
@@ -78,15 +95,45 @@ export default function CharacterControlsEditDialog(props: PropsType) {
         placeholder="Kobold"
         error={highlight && !race}
       />
+      <TextField
+        fullWidth
+        size="small"
+        label="Faction and reknown"
+        value={faction}
+        onChange={(e) => {
+          setFaction(e.target.value);
+        }}
+        placeholder="Lords Alliance (Reknown level 2)"
+      />
+      <TextField
+        fullWidth
+        size="small"
+        label="Campaign"
+        value={campaign}
+        onChange={(e) => {
+          setCampaign(e.target.value);
+        }}
+      />
       <Divider sx={{ width: "95%" }}>External Links</Divider>
       <TextField
         fullWidth
+        size="small"
         label="Link to character sheet"
         value={sheet}
         onChange={(e) => {
           setSheet(e.target.value);
         }}
       />
+      <TextField
+        fullWidth
+        size="small"
+        label="Link to bastion map"
+        value={character.bastion}
+        onChange={(e) => {
+          setBastion(e.target.value);
+        }}
+      />
+
       <Box
         sx={{ width: "60%" }}
         onMouseOver={() => setHighlight(true)}
@@ -99,7 +146,10 @@ export default function CharacterControlsEditDialog(props: PropsType) {
             !name ||
             (race === character.race &&
               name === character.name &&
-              sheet === character.sheet)
+              sheet === character.sheet &&
+              faction === character.faction &&
+              campaign === character.campaign &&
+              bastion === character.bastion)
           }
           onClick={handleSave}
           fullWidth
