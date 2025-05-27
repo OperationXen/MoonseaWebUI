85fe4b1559ec9019369487f6ed81914b73625fd8
Daemoxia <operationxen@apocryphasecurity.com>
2025-05-27 15:07:29 +0100

    Cloned over magic items component (#564)

--- src/components/items/commonitems/CommonItemsGrid.tsx
+++ src/components/items/commonitems/CommonItemsGrid.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import React, { ChangeEvent, useState } from "react";
+
 import { useRouter } from "next/navigation";
 
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import TextsmsIcon from "@mui/icons-material/Textsms";
 import DescriptionIcon from "@mui/icons-material/Description";
+import DisabledByDefaultIcon from "@mui/icons-material/DisabledByDefault";
 
-import { Box, Checkbox, IconButton, Button } from "@mui/material";
-import { Tooltip, Typography } from "@mui/material";
-import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
-import { DataGrid, GridPagination } from "@mui/x-data-grid";
+import { Box, Checkbox, IconButton } from "@mui/material";
+import { Button, Tooltip, Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridPagination } from "@mui/x-data-grid";
+import { GridRenderCellParams, GridRowParams } from "@mui/x-data-grid";
 
 import { useMagicItems } from "@/data/fetch/items/magicitems";
 import { getNumberEquipped } from "@/utils/items";
 import useSnackbar from "@/data/store/snackbar";
 
+import CreateAdvertDialog from "@/components/trade/CreateAdvertDialog";
+import ItemMarketWidget from "../widgets/ItemMarketWidget";
 import NoItemsOverlay from "../widgets/NoItemsOverlay";
 import ConfirmItemDelete from "../ConfirmItemDelete";
 import RarityWidget from "../widgets/RarityWidget";
@@ -30,21 +33,30 @@ type PropsType = {
   editable: boolean;
 };
 
-export function CommonItemsGrid(props: PropsType) {
+export function MagicItemsGrid(props: PropsType) {
   const { characterUUID, editable } = props;
 
   const router = useRouter();
   const { displayMessage } = useSnackbar();
-  const { data, updateItem, deleteItem } = useMagicItems(characterUUID);
+  const { data, updateItem, deleteItem, refreshItems } =
+    useMagicItems(characterUUID);
 
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [editConsumable, setEditConsumable] = useState<MagicItem | null>(null);
+  const [hideMarket, setHideMarket] = useState(true);
+  const [editItem, setEditItem] = useState<MagicItem | null>(null);
   const [deleteConfirmItem, setDeleteConfirmItem] = useState<MagicItem>();
+  const [advertItem, setAdvertItem] = useState<MagicItem>();
 
-  // filter items down to just the common
+  // filter out uncommon+ items, and anything in the trading post
   const magicItems = data?.filter((item: MagicItem) => {
-    return item.rarity === "common";
+    if (item.market && hideMarket) return false;
+    return ["common"].includes(item.rarity);
   });
+
+  const getRowFormat = (p: GridRowParams) => {
+    if (p.row.market) return "bg-gray-400 opacity-70";
+    return "";
+  };
 
   const handleItemDelete = (item: MagicItem | undefined) => {
     if (!item) return;
@@ -57,15 +69,25 @@ export function CommonItemsGrid(props: PropsType) {
   const renderEquipped = (p: GridRenderCellParams<MagicItem>) => {
     return (
       <Box className="flex items-center h-full w-full justify-center">
-        <Checkbox
-          checked={p.row.equipped}
-          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-            updateItem({
-              uuid: p.row.uuid,
-              equipped: !!event.target.checked,
-            });
-          }}
-        />
+        {(p.row.market && (
+          <Tooltip
+            title="This item is in the trading post, if you want to equip it you must first remove it from the trading post"
+            placement="left"
+          >
+            <DisabledByDefaultIcon className="opacity-80" />
+          </Tooltip>
+        )) || (
+          <Checkbox
+            disabled={p.row.market}
+            checked={p.row.equipped}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              updateItem({
+                uuid: p.row.uuid,
+                equipped: !!event.target.checked,
+              });
+            }}
+          />
+        )}
       </Box>
     );
   };
@@ -74,10 +96,19 @@ export function CommonItemsGrid(props: PropsType) {
     if (!p.row.editable) return null;
     return (
       <Box className="flex items-center justify-end">
+        <ItemMarketWidget
+          item={p.row}
+          charUUID={characterUUID}
+          onAdd={() => {
+            setAdvertItem(p.row);
+          }}
+          onRemove={() => {}}
+        />
+
         <IconButton onClick={() => router.push(`/magicitem/${p.row.uuid}`)}>
           <DescriptionIcon fontSize="small" />
         </IconButton>
-        <IconButton onClick={() => setEditConsumable(p.row)}>
+        <IconButton onClick={() => setEditItem(p.row)}>
           <EditIcon fontSize="small" />
         </IconButton>
         <IconButton onClick={() => setDeleteConfirmItem(p.row)}>
@@ -91,7 +122,21 @@ export function CommonItemsGrid(props: PropsType) {
     {
       field: "name",
       headerName: "Name",
-      flex: 5,
+      flex: 4,
+      renderCell: (p) => {
+        if (p.row.rp_name) {
+          return (
+            <Box className="flex w-full items-center h-full">
+              <Typography>{`${p.row.rp_name} (${p.row.name})`}</Typography>
+            </Box>
+          );
+        }
+        return (
+          <Box className="flex w-full items-center h-full">
+            <Typography>{`${p.row.name}`}</Typography>
+          </Box>
+        );
+      },
     },
     {
       field: "description",
@@ -101,7 +146,7 @@ export function CommonItemsGrid(props: PropsType) {
         return (
           <Box className="flex w-full items-center h-full justify-center">
             <Tooltip arrow placement="bottom" title={p.row.description}>
-              <TextsmsIcon
+              <DescriptionIcon
                 fontSize="small"
                 className={p.row.description ? "" : "opacity-20"}
               />
@@ -116,7 +161,7 @@ export function CommonItemsGrid(props: PropsType) {
       flex: 2,
       renderCell: (p) => {
         return (
-          <Box className="flex items-center h-full">
+          <Box className="flex items-center h-full justify-center">
             <RarityWidget rarity={p.row.rarity} text />
           </Box>
         );
@@ -124,11 +169,11 @@ export function CommonItemsGrid(props: PropsType) {
     },
     {
       field: "attunement",
-      headerName: "Attunement",
-      flex: 1,
+      headerName: `Attunement (${0})`,
+      flex: 2,
       renderCell: (p) => {
         return (
-          <Box className="flex items-center h-full">
+          <Box className="flex items-center h-full justify-center">
             <Typography>{p.row.attunement ? "Required" : ""}</Typography>
           </Box>
         );
@@ -136,7 +181,7 @@ export function CommonItemsGrid(props: PropsType) {
     },
     {
       field: "equipped",
-      headerName: `Equipped ${getNumberEquipped(magicItems)}`,
+      headerName: `Equipped (${getNumberEquipped(magicItems)})`,
       flex: 2,
       renderCell: renderEquipped,
     },
@@ -144,7 +189,7 @@ export function CommonItemsGrid(props: PropsType) {
       field: "actions",
       headerName: "Actions",
       type: "actions",
-      minWidth: 130,
+      minWidth: 160,
       headerAlign: "center",
       renderCell: getRowActions,
     },
@@ -155,9 +200,12 @@ export function CommonItemsGrid(props: PropsType) {
       <DataGrid
         autoPageSize
         getRowId={(x) => x.uuid}
+        getRowClassName={getRowFormat}
         columns={columns}
         rows={magicItems}
-        onRowDoubleClick={(p) => router.push(`/magicitem/${p.row.uuid}`)}
+        onRowDoubleClick={(p) => {
+          router.push(`/magicitem/${p.row.uuid}`);
+        }}
         initialState={{
           sorting: {
             sortModel: [{ field: "equipped", sort: "desc" }],
@@ -169,32 +217,47 @@ export function CommonItemsGrid(props: PropsType) {
         slots={{
           footer: () => (
             <Box
-              className="flex px-1 py-1 h-11 items-center"
+              className="flex px-1 h-11 items-center justify-between"
               sx={{ borderTop: "1px solid black" }}
             >
-              <Button
-                disabled={!editable}
-                sx={{ pointerEvents: "auto" }}
-                variant="outlined"
-                onClick={() => setDialogOpen(true)}
-              >
-                Add common item
-              </Button>
+              <Box>
+                <Button
+                  sx={{ pointerEvents: "auto" }}
+                  disabled={!editable}
+                  variant="outlined"
+                  onClick={() => setDialogOpen(true)}
+                >
+                  Add common item
+                </Button>
+                <Checkbox
+                  className="ml-4"
+                  checked={hideMarket}
+                  onChange={() => setHideMarket(!hideMarket)}
+                />
+                <Typography className="opacity-70" variant="caption">
+                  Hide items in trading post
+                </Typography>
+              </Box>
               <GridPagination className="flex items-center justify-end no-scrollbar overflow-hidden" />
             </Box>
           ),
           noRowsOverlay: NoItemsOverlay as any,
         }}
+        slotProps={{
+          noRowsOverlay: {
+            additional: "By default items in the trading post are not shown",
+          } as any,
+        }}
         density="compact"
       />
       <MagicItemDialog
-        open={dialogOpen || !!editConsumable}
+        open={dialogOpen || !!editItem}
         onClose={() => {
           setDialogOpen(false);
-          setEditConsumable(null);
+          setEditItem(null);
         }}
         characterUUID={characterUUID}
-        item={editConsumable}
+        item={editItem}
         defaultRarity="common"
       />
       <ConfirmItemDelete
@@ -202,8 +265,14 @@ export function CommonItemsGrid(props: PropsType) {
         onClose={() => setDeleteConfirmItem(undefined)}
         onConfirm={() => handleItemDelete(deleteConfirmItem)}
       />
+      <CreateAdvertDialog
+        item={advertItem}
+        onCreate={() => refreshItems()}
+        onClose={() => setAdvertItem(undefined)}
+        open={!!advertItem}
+      />
     </React.Fragment>
   );
 }
 
-export default CommonItemsGrid;
+export default MagicItemsGrid;
